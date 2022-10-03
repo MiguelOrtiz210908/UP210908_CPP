3147204a1d10dbcc89a1b15240fc7664ec03745c
UP210908 <up210908@alumnos.upa.edu.mx>
2022-10-02 20:37:41 -0500

    Practica While

---
+++ U3/04_While.cpp
@@ -0,0 +1,27 @@
+/* Unidad 2. Usos de las condicionales
+   Autor: Miguel Angel Ortiz
+   Fecha: 26/09/2022
+   Descripción: Muestra la forma de ingresar tipos de datos por el usuario
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+//--------Declaracion de variables--------
+    int contador;
+//--------Inicializacion del contador-----
+    contador=1;
+
+    while(contador <=15){
+//--------Imprime el valor del contador-----
+    cout<<"El contador es: "<<contador<<endl;
+//--------Incremento del contador--------
+    contador++;
+
+    }    
+    return 0;
+
+}
