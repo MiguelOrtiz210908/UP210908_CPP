2cb4924263c53af765ceecdfe48e3a1322030d64
UP210908 <up210908@alumnos.upa.edu.mx>
2022-10-07 09:51:01 -0500

    Archivos

---
+++ U2/07_BinarioaDecimal.cpp
@@ -0,0 +1,41 @@
+/* Unidad 3. 
+   Autor: Miguel Angel Ortiz
+   Fecha: 05/10/2022
+   Descripción: Muestra la forma de ingresar tipos de datos por el usuario
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    string  resultado;
+
+    do{
+        cout<<"Ingrese el numero a convertir: ";
+        cin>>num;
+
+        if(num>0){
+            cout<<"Mayor a cero"<<endl;
+            while(num!=0){
+                if(num%2==0){
+                    resultado='0'+resultado;
+                }
+                else{
+                    resultado = '1' + resultado;
+                }
+
+                num/=2;
+        
+        }
+        }
+        else if(num==0){
+            cout<<"0"<<endl;
+        }
+        else{
+            cout<<"Menor a cero"<<endl;
+        }
+    }while(num<0);
+    cout<<resultado<<endl;
+}
