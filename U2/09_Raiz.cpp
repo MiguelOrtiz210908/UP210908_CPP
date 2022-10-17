c4af1b5e5a6f22e81a8522f139ed54ace62bf773
UP210908 <up210908@alumnos.upa.edu.mx>
2022-10-16 21:37:30 -0500

    Archivo

--- U2/09_Raiz.cpp
+++ U2/09_Raiz.cpp
@@ -1,39 +1,94 @@
-/* Unidad 3. 
-   Autor: Miguel Angel Ortiz
-   Fecha: 05/10/2022
-   Descripción: Muestra la forma de ingresar tipos de datos por el usuario
+/*
+Date: 13/10/2022
+Author: Héctor Iván Ramírez Jaime.
+Email: up210052@alumnos.upa.edu.mx
+Description: Metodo de bisección.
+
+Problem (Spanish)
+Hacer el metodo de la bisección de y=x^2 + 3x + 8 en un margen de 0.01 al -0.01
 */
 
+// Library for input and output management.
+#include <iostream>
+#include <cmath>
 
-#include<iostream>
-#include<cmath>
+// Library to be able to print only selected digits
+#include <iomanip>
 
+// Use of namespace to avoid std::
 using namespace std;
-
-float ResolverEcuacion(float valor){
-    return pow(valor, 2) + 3*(valor)+8;
+// Function to solve the equation
+float resolverEcuacion(float numero)
+{
+    return (pow(numero, 2) - numero - 12);
 }
 
-int main(){
+// Function to print the line of -
+int imprimirlinea()
+{
+    for (int i = 0; i < 161; i++)
+    {
+        cout << "-";
+    }
+    cout << "\n";
+    return 0;
+}
 
-    float a;
-    float b;
-    float c=(a+b)/2;
-    float ya= ResolverEcuacion(a);
-    float yb= ResolverEcuacion(b);
-    float yc= ResolverEcuacion(c);
+int main()
+{
+    // Declare the variables.
+    int interador = 1;
+    int solucion = 1;
+    float a, b, c, ya, yb, yc = 1;
 
-    cout<<"Ingresa el valor de a: ";
-    cin>>a;
-    cout<<"Ingresa el valor de b: ";
-    cin>>b;
-    c=(a+b)/2;
-    yc= ResolverEcuacion(c);
-    cout<<yc;
+    // Ask the user for the range
+    cout << "Enter the value A: ";
+    cin >> a;
+    cout << "Enter the value B: ";
+    cin >> b;
 
-    while(yc>=0.001 || yc<=-0.001){
-        
+    // Print table header
+    imprimirlinea();
+    cout << "| Interdor \t| a \t| b \t| c \t| y(a)\t| y(b)\t| y(c)\t| \n";
+    imprimirlinea();
 
+    while (yc >= 0.01 || yc <= -0.01)
+    {
+        // Obtain point c and Y(a), Y(b), Y(c)
+        c = (a + b) / 2;
+        ya = resolverEcuacion(a);
+        yb = resolverEcuacion(b);
+        yc = resolverEcuacion(c);
+        interador++;
+        // Print the results in a table
+        cout << "| " << interador << "\t\t| " << fixed << setprecision(8) << a << "\t\t| " << b << "\t\t| " << c << "\t\t| " << ya << "\t\t| " << yb << "\t\t| " << yc << "\t\t| \n";
+        imprimirlinea();
+        // If you select which half to grab to return to get the point c and Y(a), Y(b), Y(c)
+        if ((ya * yc) < 0)
+        {
+            b = c;
+        }
+        else
+        {
+            a = c;
+        }
+        // If to prevent infinite cycling
+        if (a == b)
+        {
+            solucion = 0;
+            break;
+        }
     }
 
+    // Print the root if there is
+    if (solucion != 0)
+    {
+        cout << "The root is :" << c;
+        cout << "\n";
+    }
+    else
+    {
+        cout << "There is no root in that range. \n";
+    }
+    return 0;
 }
